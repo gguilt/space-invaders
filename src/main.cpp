b61e2accb1c0e899707c9f2c5ad6d20bcf3f3e32
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-07-01 19:15:52 +0300

    Create Game class

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,113 @@
-#include <iostream>
+// Space Invaders SDL2 by Radon Therapy
+// 2023-07-01
+
+// Header files
+#include <SDL2/SDL.h>
+
+// Variables and constants
+
+// - Window settings
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define FRAME_DELAY_MS (1000 / 60) // 60 FPS
+
+// Game class
+class Game {
+public:
+    Game() : window(nullptr), renderer(nullptr) {}
+
+    bool Initialize()
+    {
+        // Initialize SDL
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[error] Couldn't initialize SDL2: %s", SDL_GetError());
+            return false;
+        }
+
+        // Create window
+        window = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == nullptr) {
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[error] Couldn't create window: %s", SDL_GetError());
+            SDL_Quit();
+            return false;
+        }
+
+        // Create renderer
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if (renderer == nullptr) {
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create renderer: %s", SDL_GetError());
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return false;
+        }
+
+        return true;
+    }
+
+    void Run()
+    {
+        bool quit = false;
+        SDL_Event event;
+
+        Uint32 frameStart, frameTime;
+
+        while (!quit) {
+            frameStart = SDL_GetTicks();
+
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_QUIT) {
+                    quit = true;
+                }
+            }
+
+            Update();
+            Render();
+
+            // Delay leftover time
+            frameTime = SDL_GetTicks() - frameStart;
+            if (FRAME_DELAY_MS > frameTime) {
+                SDL_Delay(FRAME_DELAY_MS - frameTime);
+            }
+        }
+    }
+
+    void Update() {
+        // Update game
+    }
+
+    void Render()
+    {
+        // Clean-up
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Update screen
+        SDL_RenderPresent(renderer);
+    }
+
+    void Shutdown()
+    {
+        // Final clean-up and shutdown
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+
+        SDL_Quit();
+    }
+
+private:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+};
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello world" << std::endl;
-    return 1;
+    Game game;
+
+    if (game.Initialize()) {
+        game.Run();
+    }
+
+    game.Shutdown();
+
+    return 0;
 }
